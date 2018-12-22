64f3c6c4e73d8fcc64a86d88b87cda145ba57585
Venkatesvaran G <sharpegv@gmail.com>
2018-12-22 19:16:32 +0800

    added file upload support

--- public/js/chat.js
+++ public/js/chat.js
@@ -32,6 +32,21 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(html);
 });
 
+socket.on('newFile', function(data) {
+  console.log('New File', data);
+  var formattedTime = moment(data.createAt).format('h:mm a');
+
+  var template = jQuery('#file-message-template').html();
+  var html = Mustache.render(template, {
+    from : data.from,
+    filename: data.filename,
+    size : data.size,
+    createAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+});
+
 socket.on('updatedUserList', function(users) {
   console.log(users);
   var ol = jQuery('<ol></ol>');
@@ -52,3 +67,78 @@ jQuery('#form').on('submit', function (e) {
     msgTxtBox.val('');
   });
 });
+
+jQuery('#myFileInput').on('change', function (e) {
+  console.log('Upload on change...' + e.target.files);
+  var files = e.target.files;
+  if (files && files.length) {
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      console.log('file.name...' + file.name);
+      var dateString = formatAMPM(new Date());
+      var DWid = params.name + "dwid" + Date.now();
+
+      var fd = new FormData();
+    	fd.append('file', file);
+      fd.append('username', params.name);
+      fd.append('usergroup', params.group);
+			fd.append('dwid', DWid);
+			fd.append('msgTime', dateString);
+			fd.append('filename', file.name);
+
+      jQuery.ajax({
+        url: '/api/v1/upload',
+        type: 'POST',
+        data: fd,
+        cache: false,
+        datatype: 'json',
+        processData: false,
+        contentType: false,
+        success:
+          function(data, status, jqXHR) {
+            console.log('File upload succeded...');
+          },
+        error:
+          function(jqXHR, status, error) {
+            console.log('File upload failed...' + error);
+          }
+      });
+    }
+  }
+  });
+
+function download(fileName) {
+
+  var fd = new FormData();
+  fd.append('filename', file.name);
+
+  jQuery.ajax({
+    url: '/api/v1/getFile',
+    type: 'POST',
+    data: fd,
+    cache: false,
+    datatype: 'json',
+    processData: false,
+    contentType: false,
+    success:
+      function(data, status, jqXHR) {
+        console.log('File download succeded...');
+      },
+    error:
+      function(jqXHR, status, error) {
+        console.log('File download failed...' + error);
+      }
+  });
+}
+
+// message time formatting into string
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
--- server/server.js
+++ server/server.js
@@ -2,18 +2,45 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
+const formidable = require('formidable');		// file upload module
+const bodyParser = require('body-parser');	// body-parser module for reading request body
+const moment = require('moment')
 
 const {generateMessge} = require("./util/message");
 const {Users} = require("./util/users");
 
 const publicpath = path.join(__dirname, '../public');
+const publicUploadPath = path.join(__dirname, '../public/upload');
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 var users = new Users();
 
+// cofiguring body-parser
+app.use(bodyParser.json({	// setting json limit
+    limit: 1024 * 10000
+}));
+app.use(bodyParser.text({ 	// setting text limit
+    limit: 1024 * 10000
+}));
+app.use(bodyParser.raw({ 	// setting raw limit
+    limit: 1024 * 10000
+}));
+app.use(bodyParser.urlencoded({		// setting url encoding
+    extended: true
+}));
+
 app.use(express.static(publicpath));
+app.use(express.static(publicUploadPath));
+
+// CORS Issue Fix
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 io.on('connect', (socket) => {
   console.log('New user connected');
@@ -45,6 +72,55 @@ io.on('connect', (socket) => {
     }
   });
 });
+
+//route for uploading files asynchronously
+app.post('/api/v1/upload',function (req, res) {
+
+  console.log('Calling api/v1/upload...');
+
+  var imgdatetimenow = Date.now();
+  var form = new formidable.IncomingForm({
+        uploadDir: publicUploadPath,
+        keepExtensions: true
+      });
+
+  form.on('end', function() {
+      res.end();
+    });
+
+  form.parse(req, function(err, fields, files) {
+    var data = {
+        from : fields.username,
+        filename : baseName(files.file.path),
+        size : bytesToSize(files.file.size),
+        createAt : moment.valueOf()
+    };
+
+    console.log("new file:" + fields.usergroup);
+    io.to(fields.usergroup).emit('newFile', data);
+  });
+});
+
+
+// route for checking requested file , does exist on server or not
+app.post('/v1/getfile', function(req, res){
+});
+
+//get file name from server file path
+function baseName(str) {
+   var base = new String(str).substring(str.lastIndexOf('\\') + 1);
+   return base;
+}
+
+// Size Conversion
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    if (i == 0) return bytes + ' ' + sizes[i];
+    return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+};
+
 
 server.listen (3000, () => {
   console.log("Server started on port 3000");
